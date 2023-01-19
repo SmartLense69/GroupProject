94cffe2a3029f171ed2ebc6354032292ff74ad7a
OliviaS <olivia.simon2000@gmail.com>
2023-01-19 13:10:24 +0000

    rainbows

--- RK4.py
+++ RK4.py
@@ -23,25 +23,25 @@ def RK4(func, xi1, phi0, theta0, n, h, *arg):
 
     xi = np.arange(0, xi1 + h, h)
     # number of time steps
-    n = np.shape(xi)[0]
+    steps = np.shape(xi)[0]
 
-    phi = np.zeros(n)
-    theta = np.zeros(n)
+    phi = np.zeros(steps)
+    theta = np.zeros(steps)
 
     # set the initial conditions
     phi[0] = phi0
     theta[0] = theta0
 
     # Loop over time
-    for i in range(1, n):
+    for i in range(1, steps):
 
         # find phi using RK4 method
 
         # calculate the values k1 through k4
-        k1 = h * func(xi[i - 1] + c[1]*h, phi[i - 1], theta[i -1], n, *arg)
-        k2 = h * func(xi[i - 1] + c[2]*h, phi[i - 1] + a[2, 1]*k1, theta[i -1], n, *arg)
-        k3 = h * func(xi[i - 1] + c[3]*h, phi[i - 1] + a[3, 1]*k1 + a[3, 2]*k2, theta[i -1], n, *arg)
-        k4 = h * func(xi[i - 1] + c[4]*h, phi[i - 1] + a[4, 1]*k1 + a[4, 2]*k2 + a[4, 3]*k3, theta[i -1], n, *arg)
+        k1 = h * func(xi[i - 1] + c[1]*h, phi[i - 1], n, theta[i -1], *arg)
+        k2 = h * func(xi[i - 1] + c[2]*h, phi[i - 1] + a[2, 1]*k1, n, theta[i -1], *arg)
+        k3 = h * func(xi[i - 1] + c[3]*h, phi[i - 1] + a[3, 1]*k1 + a[3, 2]*k2, n, theta[i -1], *arg)
+        k4 = h * func(xi[i - 1] + c[4]*h, phi[i - 1] + a[4, 1]*k1 + a[4, 2]*k2 + a[4, 3]*k3, n, theta[i -1], *arg)
 
         # calculate the next value for phi
         phi[i] = phi[i - 1] + b[1]*k1 + b[2]*k2 + b[3]*k3 + b[4]*k4
@@ -52,14 +52,21 @@ def RK4(func, xi1, phi0, theta0, n, h, *arg):
     return (xi, theta)
 
 # outputs for different values of n
-xi0, theta0 = RK4(phigrad, 1, 0, 1, 0, 0.01)
-xi1, theta1 = RK4(phigrad, 1, 0, 1, 1, 0.01)
-xi5, theta5 = RK4(phigrad, 1, 0, 1, 5, 0.01)
+n_list = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5]
+colours = ['r', 'orange', 'yellow', 'lightgreen', 'g', 'cyan', 'b', 'purple', 'pink', 'k', 'gray']
 
-# plot
-plt.plot(xi0, theta0, label='n=0')
-plt.plot(xi1, theta1, label='n=1')
-plt.plot(xi5, theta5, label='n=5')
+# plot configs
+plt.figure(figsize=(12, 8))
+
+for n, c in zip(n_list, colours):
+
+    # run RK4
+    xi, theta = RK4(phigrad, 1, 0, 1, n, 0.01)
+
+    # plot curve
+    plt.plot(xi, theta, c=c, label='n={0}'.format(n))
+
+# plot configs
 plt.title('Euler Lane-Emden')
 plt.xlabel(r'$\xi$')
 plt.ylabel(r'$\theta$')
