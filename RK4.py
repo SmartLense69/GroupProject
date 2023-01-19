4d0ec137846712b7ae03c31296c8cd0c51d2adb7
OliviaS <olivia.simon2000@gmail.com>
2023-01-19 12:50:20 +0000

    RK4 supposed to be done

--- RK4.py
+++ RK4.py
@@ -5,18 +5,18 @@ def phigrad(xi, phi, n, theta):
     if xi==0:
         out = 0
     else:
-        out = ((-2/xi)*phi0) - theta0**n
+        out = ((-2/xi)*phi) - theta**n
     return out
 
 def RK4(func, xi1, phi0, theta0, n, h, *arg):
 
     # constants
     c = np.array([None, 0, 1/2, 1/2, 1])
-    a = np.array([None, None, None, None],
+    a = np.array([[None, None, None, None],
                  [None, None, None, None],
-                 [None, 1/2, 0, 0],
-                 [None, None, 1/2, 0],
-                 [None, None, None, 1])
+                 [None, 1/2, None, None],
+                 [None, 0, 1/2, None],
+                 [None, 0, 0, 1]])
     b = np.array([None, 1/6, 1/3, 1/3, 1/6])
 
     # initialise the arrays to be used
@@ -44,7 +44,7 @@ def RK4(func, xi1, phi0, theta0, n, h, *arg):
         k4 = h * func(xi[i - 1] + c[4]*h, phi[i - 1] + a[4, 1]*k1 + a[4, 2]*k2 + a[4, 3]*k3, theta[i -1], n, *arg)
 
         # calculate the next value for phi
-        phi[i] = phi[i - 1] + b1*k1 + b2*k2 + b3*k3 + b4*k4
+        phi[i] = phi[i - 1] + b[1]*k1 + b[2]*k2 + b[3]*k3 + b[4]*k4
 
         # find theta using simple Euler method
         theta[i] = theta[i - 1] + h * phi[i]
