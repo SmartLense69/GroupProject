cda972e92e3f21dcdfe4da15fef26a1a7337a215
andie-swain <andieswain2002@gmail.com>
2023-01-19 16:19:32 +0000

    hydrostatic still doesn't work :(

--- eulerhydro.py
+++ eulerhydro.py
@@ -2,12 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import constants as consts
 
+
+K = 1
+
 def rhograd(phi0):
     out = phi0
     return out
 
-def phigrad(r, n, rho0, phi0, G, K):
-    out = ((-((1/n)-1)/(rho0))*phi0**2)-((2/r)*rho0)-((rho0**(2/(1/n)))*G*4*np.pi / K*(1+(1/n)))
+def phigrad(r, n, rho0, phi0):
+    out = ((-((1/n)-1)/(rho0))*phi0**2)-((2/r)*phi0)-((rho0**(2/(1/n)))*consts.G*4*np.pi / K*(1+(1/n)))
     return out
 
 def euler1(h, n, func1, ic1, func2=None, ic2=None):
@@ -23,23 +26,23 @@ def euler1(h, n, func1, ic1, func2=None, ic2=None):
     :return: array of xi values and their corresponding theta values
     """
 
-    rvalues = np.arange(0, 7e6+h, h)
+    rvalues = np.arange(0, (7e6)+h, h)
     steps = len(rvalues)
-    rhosol = np.arange(0, 7e6+h, h)
-    phisol = np.arange(0, 7e6+h, h)
+    rhosol = np.arange(0, (7e6)+h, h)
+    phisol = np.arange(0, (7e6)+h, h)
     # defining inital conditions
     rhosol[0] = ic2
     phisol[0] = ic1
 
     for i in range(1, steps):
         if func2 is not None:
-            phisol[i] = phisol[i-1] + h*func1(rvalues[i-1], n, rhosol[i-1], phisol[i-1], consts.G, 1)
+            phisol[i] = phisol[i-1] + h*func1(rvalues[i-1], n, rhosol[i-1], phisol[i-1])
         rhosol[i] = rhosol[i-1] + h*func2(phisol[i-1])
 
     return rvalues, rhosol
 
 
-result = euler1(10, 1, phigrad, 0, rhograd, 1e9)
+result = euler1(100, 1, phigrad, 0, rhograd, 1e9)
 plt.plot(result[0], result[1])
 plt.xlabel('r')
 plt.ylabel(r'$\rho$')
