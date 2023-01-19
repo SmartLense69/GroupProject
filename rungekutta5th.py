501087e5bf8f4b0e699b93d89f137157a7dfea97
marcbeling <marc.beling@advanced.de>
2023-01-19 12:18:52 +0000

    Added plots for n=0,1,5 and 1<n<=5

--- rungekutta5th.py
+++ rungekutta5th.py
@@ -1,5 +1,5 @@
 import numpy as np
-import DiffEquation as DE
+import diffequation as DE
 
 
 A = np.asarray([
@@ -79,15 +79,16 @@ def rkf(diffEq1: DE.DifferentialEquation, diffEq2: DE.DifferentialEquation, step
         # Solve second differential equation.
         # There is no xi given, so only the y value of the function is altered
 
-        k1 = stepSize * diffEq2.func(phi[i])
-        k2 = stepSize * diffEq2.func(phi[i] + A[2, 1] * k1)
-        k3 = stepSize * diffEq2.func(phi[i] + A[3, 1] * k1 + A[3, 2] * k2)
-        k4 = stepSize * diffEq2.func(phi[i] + A[4, 1] * k1 + A[4, 2] * k2 + A[4, 3] * k3)
-        k5 = stepSize * diffEq2.func(phi[i] + A[5, 1] * k1 + A[5, 2] * k2 + A[5, 3] * k3
-                                     + A[5, 4] * k4)
-        k6 = stepSize * diffEq2.func(phi[i] + A[6, 1] * k1 + A[6, 2] * k2 + A[6, 3] * k3
-                                     + A[6, 4] * k4 + A[6, 5] * k5)
+        theta[i] = theta[i - 1] + stepSize * phi[i]
 
-        theta[i] = theta[i - 1] + B[1] * k1 + B[2] * k2 + B[3] * k3 + B[4] * k4 + B[5] * k5 + B[6] * k6
+        # k1 = stepSize * diffEq2.func(phi[i])
+        # k2 = stepSize * diffEq2.func(phi[i] + A[2, 1] * k1)
+        # k3 = stepSize * diffEq2.func(phi[i] + A[3, 1] * k1 + A[3, 2] * k2)
+        # k4 = stepSize * diffEq2.func(phi[i] + A[4, 1] * k1 + A[4, 2] * k2 + A[4, 3] * k3)
+        # k5 = stepSize * diffEq2.func(phi[i] + A[5, 1] * k1 + A[5, 2] * k2 + A[5, 3] * k3
+        #                            + A[5, 4] * k4)
+        # k6 = stepSize * diffEq2.func(phi[i] + A[6, 1] * k1 + A[6, 2] * k2 + A[6, 3] * k3
+        #                            + A[6, 4] * k4 + A[6, 5] * k5)
+        # theta[i] = theta[i - 1] + B[1] * k1 + B[2] * k2 + B[3] * k3 + B[4] * k4 + B[5] * k5 + B[6] * k6
 
     return xi, phi, theta
