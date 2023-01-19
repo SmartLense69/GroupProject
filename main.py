7156047c78306d0eec2fdbdd30d064ff383bbb1c
marcbeling <marc.beling@advanced.de>
2023-01-19 11:53:27 +0000

    Fixed RKF and added documentation.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import DiffEquation as DE
 import RKF
 import matplotlib.pyplot as plt
 
-N = 1.5
+N = 1
 
 
 def diff1(xi, phi, theta):
@@ -14,9 +14,9 @@ def diff2(phi):
 
 
 if __name__ == '__main__':
-    diffEq1 = DE.DifferentialEquation(diff1, 1)
-    diffEq2 = DE.DifferentialEquation(diff2, 0)
-    xi, theta, phi = RKF.rkf(diffEq1, diffEq2, 0.1, 1)
+    diffEq1 = DE.DifferentialEquation(diff1, 0)
+    diffEq2 = DE.DifferentialEquation(diff2, 1)
+    xi, phi, theta = RKF.rkf(diffEq1, diffEq2, 0.01, 1)
 
     plt.title("Solution to Lane-Emden")
     plt.plot(xi, theta)
