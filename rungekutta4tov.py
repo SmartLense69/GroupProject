6f9d7db178d8bd1e8b007f27adfade805d9fe7ab
OliviaS <olivia.simon2000@gmail.com>
2023-01-20 17:15:45 +0000

    it's doing something, probably incorrectly lol, have fun with my wonky ass code

--- rungekutta4tov.py
+++ rungekutta4tov.py
@@ -2,14 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # this is so that the state functions can be varied
-# to be implemented at a later point
 def statefunc(P, K, n):
     return (P / K)**(n/(n+1))
 
 def masscont(r, P, K, n):
     return 4 * np.pi * r**2 * statefunc(P, K, n)
 
-def hydro(r, P, m, G, K, n):
+def hydro(r, P, m, G, c, K, n):
     return - ( G * m * statefunc(P, K, n) / r**2 )
 
 def tov(r, P, m, G, c, K, n):
@@ -18,7 +17,7 @@ def tov(r, P, m, G, c, K, n):
              ( 1 + (4 * np.pi * r**3 * P) / (m * c**2) ) * \
              ( 1 - (2 * G * m) / (r * c**2) )**(-1)
 
-def rungekutta4tov(masscont, starfunc, r1, m0, P0, G, K, n, h, *arg):
+def rungekutta4tov(masscont, starfunc, r1, m0, P0, G, c0, K, n, h, *arg):
 
     # constants
     c = np.array([None, 0, 1/2, 1/2, 1])
@@ -31,7 +30,7 @@ def rungekutta4tov(masscont, starfunc, r1, m0, P0, G, K, n, h, *arg):
 
     # initialise the arrays to be used
 
-    r = np.arange(0, r1 + h, h)
+    r = np.arange(1, r1 + h, h)
     # number of time steps
     steps = np.shape(r)[0]
 
@@ -57,10 +56,10 @@ def rungekutta4tov(masscont, starfunc, r1, m0, P0, G, K, n, h, *arg):
 
         # find P using RK4 method
         # calculate the values k1 through k4
-        k1 = h * starfunc(r[i - 1] + c[1] * h, P[i - 1], m[i], G, K, n, *arg)
-        k2 = h * starfunc(r[i - 1] + c[2] * h, P[i - 1] + a[2, 1] * k1, m[i], G, K, n, *arg)
-        k3 = h * starfunc(r[i - 1] + c[3] * h, P[i - 1] + a[3, 1] * k1 + a[3, 2] * k2, m[i], G, K, n, *arg)
-        k4 = h * starfunc(r[i - 1] + c[4] * h, P[i - 1] + a[4, 1] * k1 + a[4, 2] * k2 + a[4, 3] * k3, m[i], G, K, n, *arg)
+        k1 = h * starfunc(r[i - 1] + c[1] * h, P[i - 1], m[i], G, c0, K, n, *arg)
+        k2 = h * starfunc(r[i - 1] + c[2] * h, P[i - 1] + a[2, 1] * k1, m[i], G, c0, K, n, *arg)
+        k3 = h * starfunc(r[i - 1] + c[3] * h, P[i - 1] + a[3, 1] * k1 + a[3, 2] * k2, m[i], G, c0, K, n, *arg)
+        k4 = h * starfunc(r[i - 1] + c[4] * h, P[i - 1] + a[4, 1] * k1 + a[4, 2] * k2 + a[4, 3] * k3, m[i], G, c0, K, n, *arg)
 
         # find next value for m
         P[i] = P[i - 1] + b[1] * k1 + b[2] * k2 + b[3] * k3 + b[4] * k4
@@ -77,14 +76,25 @@ plt.figure(figsize=(12, 8))
 for n, c in zip(n_list, colours):
 
     # run RK4
-    r, P = rungekutta4tov(phigrad, 3, 0, 1, n, 0.01)
+    r, P = rungekutta4tov(masscont, tov, 0.5e9, 1, 1e20, 6.67e-11, 3e8, 1e9, n, 10000)
+
+    # switch to density
+    #rho = (P / 1e9)**(n/(n+1))
 
     # plot curve
     plt.plot(r, P, c=c, label='n={0}'.format(n))
 
+'''
+# run RK4
+r, P = rungekutta4tov(masscont, hydro, 7e9, 1, 1e20, 6.67e-11, 3e8, 1e9, 1.5, 10000)
+#r, P = rungekutta4tov(masscont, tov, 700, 1, 5000, 6.67e-3, 3, 1, 1.5, 1)
+# plot curve
+plt.plot(r, P, c='r', label='n=1.5')
+'''
+
 # plot configs
-plt.title('Euler Lane-Emden')
-plt.xlabel(r'$\xi$')
-plt.ylabel(r'$\theta$')
+plt.title('RK4 hydrostatic')
+plt.xlabel('r')
+plt.ylabel('P')
 plt.legend()
 plt.show()
