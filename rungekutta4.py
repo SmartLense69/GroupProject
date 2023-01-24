2b8180a857bcbac74c382315cc57a194ce75337e
OliviaS <olivia.simon2000@gmail.com>
2023-01-24 18:06:45 +0000

    started writing, had a mental breakdown, bon appétit!

--- rungekutta4.py
+++ rungekutta4.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 
 # this is so that the state functions can be varied
 def statefunc(K, n, P=None, rho=None):
-    if P != None:
-        return (P / K)**(n/(n+1))
+    if P is not None:
+        return (np.abs(P) / K)**(n/(n+1))
     if rho != None:
         return K * rho**(1 + 1/n)
 
@@ -51,25 +51,31 @@ def rungekutta4(masscont, starfunc, r1, m0, rho0, G, c0, K, n, h, *arg):
 
         # find m using RK4 method
         # calculate the values k1 through k4
-        k1 = h * masscont(r[i] + c[1]*h, P[i - 1], K, n, *arg)
-        k2 = h * masscont(r[i] + c[2]*h, P[i - 1], K, n, *arg)
-        k3 = h * masscont(r[i] + c[3]*h, P[i - 1], K, n, *arg)
-        k4 = h * masscont(r[i] + c[4]*h, P[i - 1], K, n, *arg)
+        k1 = h * masscont(r[i] + c[1]*h, np.abs(P[i - 1]), K, n, *arg)
+        k2 = h * masscont(r[i] + c[2]*h, np.abs(P[i - 1]), K, n, *arg)
+        k3 = h * masscont(r[i] + c[3]*h, np.abs(P[i - 1]), K, n, *arg)
+        k4 = h * masscont(r[i] + c[4]*h, np.abs(P[i - 1]), K, n, *arg)
 
         # find next value for m
         m[i] = m[i - 1] + b[1]*k1 + b[2]*k2 + b[3]*k3 + b[4]*k4
 
         # find P using RK4 method
         # calculate the values k1 through k4
-        k1 = h * starfunc(r[i] + c[1] * h, P[i - 1], m[i], G, c0, K, n, *arg)
-        k2 = h * starfunc(r[i] + c[2] * h, P[i - 1] + a[2, 1] * k1, m[i], G, c0, K, n, *arg)
-        k3 = h * starfunc(r[i] + c[3] * h, P[i - 1] + a[3, 1] * k1 + a[3, 2] * k2, m[i], G, c0, K, n, *arg)
-        k4 = h * starfunc(r[i] + c[4] * h, P[i - 1] + a[4, 1] * k1 + a[4, 2] * k2 + a[4, 3] * k3, m[i], G, c0, K, n, *arg)
+        k1 = h * starfunc(r[i] + c[1] * h, np.abs(P[i - 1]), m[i], G, c0, K, n, *arg)
+        k2 = h * starfunc(r[i] + c[2] * h, np.abs(P[i - 1]) + a[2, 1] * k1, m[i], G, c0, K, n, *arg)
+        k3 = h * starfunc(r[i] + c[3] * h, np.abs(P[i - 1]) + a[3, 1] * k1 + a[3, 2] * k2, m[i], G, c0, K, n, *arg)
+        k4 = h * starfunc(r[i] + c[4] * h, np.abs(P[i - 1]) + a[4, 1] * k1 + a[4, 2] * k2 + a[4, 3] * k3, m[i], G, c0, K, n, *arg)
 
         # find next value for m
-        P[i] = P[i - 1] + b[1] * k1 + b[2] * k2 + b[3] * k3 + b[4] * k4
+        P[i] = np.abs(P[i - 1]) + b[1] * k1 + b[2] * k2 + b[3] * k3 + b[4] * k4
 
-    return (r, P)
+        if P[i]/P[0] < 1e-5:
+            P = P[:i]
+            r = r[:i]
+            m = m[:i]
+            break
+
+    return (r, P, m)
 
 # outputs for different values of n
 n_list = [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5]
@@ -81,13 +87,15 @@ plt.figure(figsize=(12, 8))
 for n, c in zip(n_list, colours):
 
     # run RK4
-    r, P = rungekutta4(masscont, hydro, 5e6, 0, 1e9, 6.67e-8, 3e10, 5e11, n, 1e4)
+    r, P, m = rungekutta4(masscont, tov, 2e8, 0, 1e9, 6.67e-8, 3e10, 5e11, n, 1e5)
+    print('n:', n, 'R:', r[-1], 'M:', m[-1])
+    print()
 
     # switch to density
-    rho = (P / 5e11)**(n/(n+1))
+    rho = statefunc(5e11, n, P=P)
 
     # plot curve
-    plt.plot(r/np.max(r), rho/np.max(rho), c=c, label='n={0}'.format(n)) #rho/np.max(rho)
+    plt.plot(r/np.max(r), rho/np.max(rho), c=c, label='n={0}'.format(n))
 
 # plot configs
 plt.title('RK4 hydrostatic')
