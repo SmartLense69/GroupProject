76bafe53e63c487dcd42912d82954e6909c9b9fc
marcbeling <marc.beling@advanced.de>
2023-02-07 16:05:03 +0000

    Added Mass Radius diagram for Hydro and TOV for White dwarfs and Neutron stars

--- rhoTest.py
+++ rhoTest.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import CubicSpline
 
-
 # Global variables
 
 _M0 = 1
@@ -11,17 +10,10 @@ _C = 3e10
 _K = 1e13
 _N = 1.5
 _R = 2e10
-_PLOTPRHO = True
+_PLOTPRHO = False
 
-cubicSplineData = np.loadtxt("EoS_Spline_Data.csv", delimiter='\t').transpose()
-getRho = CubicSpline(cubicSplineData[1], cubicSplineData[0], extrapolate=True)
-getP = CubicSpline(cubicSplineData[0], cubicSplineData[1], extrapolate=True)
-
-if _PLOTPRHO:
-    _p = np.linspace(1e15, 1e19, 1000)
-    plt.plot(_p, getRho(_p))
-    plt.show()
-
+getRho = 0
+getP = 0
 
 def _M(r, P):
     return 4 * np.pi * r ** 2 * getRho(P)
@@ -29,9 +21,13 @@ def _M(r, P):
 
 def _TOV(r, P, m):
     return - ((_G * m * getRho(P)) / r ** 2) \
-        * (1 + P / (getRho(P) * _C ** 2)) \
-        * (1 + (4 * np.pi * r ** 3 * P) / (m * _C ** 2)) \
-        * (1 - (2 * _G * m) / (r * _C ** 2)) ** (-1)
+         * (1 + P / (getRho(P) * _C ** 2)) \
+         * (1 + (4 * np.pi * r ** 3 * P) / (m * _C ** 2)) \
+         * (1 - (2 * _G * m) / (r * _C ** 2)) ** (-1)
+
+
+def _HYDRO(r, P, m):
+    return - ((_G * m * getRho(P)) / (r ** 2))
 
 
 def _RK4(masscont, starfunc, statefunc, h, rho):
@@ -83,7 +79,10 @@ def _RK4(masscont, starfunc, statefunc, h, rho):
         # find next value for P
         P[i] = np.abs(P[i - 1]) + b[1] * k1 + b[2] * k2 + b[3] * k3 + b[4] * k4
 
-        if P[i]/P[0] < 1e-5:
+        if np.isnan(j4):
+            print("Warning: j4 is nan.")
+
+        if P[i]/P[0] < 1e-7:
             P = P[:i]
             r = r[:i]
             m = m[:i]
@@ -92,10 +91,73 @@ def _RK4(masscont, starfunc, statefunc, h, rho):
     return r, P, m
 
 
-def runRK4():
-    for rho in np.geomspace(1e6, 1e12, 100):
-        r, P, m = _RK4(_M, _TOV, getP, 1e5, rho)
-        plt.scatter(r[-1], m[-1], color='b', marker='v')
+def runRK4TOV(rhoMin, rhoMax, rStep, rhoNum=100, verbose=False, color='r', marker='v'):
+    radius = np.zeros(np.geomspace(rhoMin, rhoMax, rhoNum).size)
+    mass = np.zeros(np.geomspace(rhoMin, rhoMax, rhoNum).size)
+    print("i is in range {0}".format(np.geomspace(rhoMin, rhoMax, rhoNum).size))
+    for i, rho in enumerate(np.geomspace(rhoMin, rhoMax, rhoNum)):
+        r, P, m = _RK4(_M, _TOV, getP, rStep, rho)
+        if verbose:
+            print(r)
+            print(P)
+            print(m)
+        print("TOV: Calculation {0} at rho={1}".format(i, rho))
+        radius[i] = r[-1]/(1e5)
+        mass[i] = m[-1]/(2e33)
+        plt.scatter(r[-1]/(1e5), m[-1]/(2e33), color=color, marker=marker)
     plt.grid()
-    plt.show()
+
+
+def runRK4Hydro(rhoMin, rhoMax, rStep, rhoNum=100, verbose=False, color='r', marker='v'):
+    radius = np.zeros(np.geomspace(rhoMin, rhoMax, rhoNum).size)
+    mass = np.zeros(np.geomspace(rhoMin, rhoMax, rhoNum).size)
+    print("i is in range {0}".format(np.geomspace(rhoMin, rhoMax, rhoNum).size))
+    for i, rho in enumerate(np.geomspace(rhoMin, rhoMax, rhoNum)):
+        r, P, m = _RK4(_M, _HYDRO, getP, rStep, rho)
+        if verbose:
+            print(r)
+            print(P)
+            print(m)
+        print("HYDRO: Calculation {0} at rho={1}".format(i, rho))
+        radius[i] = r[-1]/(1e5)
+        mass[i] = m[-1]/(2e33)
+        plt.scatter(r[-1]/(1e5), m[-1]/(2e33), color=color, marker=marker)
+    plt.grid()
+
+
+def createSpline():
+    cubicSplineData = np.loadtxt("EoS_Spline_Data.csv", delimiter='\t').transpose()
+    global getRho
+    getRho = CubicSpline(cubicSplineData[1], cubicSplineData[0], extrapolate=True)
+    global getP
+    getP = CubicSpline(cubicSplineData[0], cubicSplineData[1], extrapolate=True)
+    if _PLOTPRHO:
+        _p = np.linspace(1e15, 1e19, 1000)
+        plt.plot(_p, getRho(_p))
+        plt.show()
+
+
+def createSLySpline():
+    cubicSplineSlyData = np.loadtxt("wrishikData/Neutron-Star-Structure-master/SLy.txt").transpose()
+    rhoData = cubicSplineSlyData[2]
+    pressureData = cubicSplineSlyData[3]
+    global getRho
+    getRho = CubicSpline(pressureData, rhoData, extrapolate=True)
+    global getP
+    getP = CubicSpline(rhoData, pressureData, extrapolate=True)
+    if _PLOTPRHO:
+        _p = np.linspace(0, 5e15, 1000)
+        plt.plot(_p, getRho(_p), color='r')
+        plt.show()
+
+
+createSpline()
+runRK4TOV(1e6, 1e14, 1e5, 30, color='r', marker='o')
+runRK4Hydro(1e6, 1e14, 1e5, 30, color='b', marker='v')
+plt.grid()
+plt.show()
+createSLySpline()
+runRK4TOV(2.5e14, 5e15, 1e3, 50, color='g', marker='x')
+plt.grid()
+plt.show()
 
